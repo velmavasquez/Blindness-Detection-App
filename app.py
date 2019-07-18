6271c0d1896a7ae44fef8523960bfcc0db216c15
angelalejandro <45617025+aalejandroq@users.noreply.github.com>
2019-07-18 14:23:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,12 @@ def crop_image1(img,tol=7):
 def load_model():
     global model
     global graph
-    # the following with stmnt may only be needed when running in tensorflow 1.13.1 environment
+    # the following with stmnt may only be needed when running in tensorflow 1.13.1 environment.
     with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
-        # model = keras.models.load_model(os.path.join(app.config['MODEL_FOLDER'], "cnn1_trained_amy.h5"))
-        model = keras.models.load_model(os.path.join(app.config['NB_FOLDER'], "colab_tf_1131_CPU.h5"))
+        model = keras.models.load_model(os.path.join(app.config['MODEL_FOLDER'], "cnn1_trained_jn.h5"))
+        #model = keras.models.load_model(os.path.join(app.config['NB_FOLDER'], "cnn1_trained.h5"))
+        #model = keras.models.load_model(os.path.join(app.config['MODEL_FOLDER'], "cnn1_trained_amy.h5"))
+        #model = keras.models.load_model(os.path.join(app.config['NB_FOLDER'], "colab_tf_1131_CPU.h5"))
     graph = K.get_session().graph
     print("  *****  Model loaded!")
 
