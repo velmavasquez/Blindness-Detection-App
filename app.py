c1d3a494f5b905f15625185dedfc297b10def200
Lizzy Clay <46448543+aeclaman@users.noreply.github.com>
2019-07-22 20:29:11 -0500

    change app.py to use new model

--- app.py
+++ app.py
@@ -16,9 +16,7 @@ app = Flask(__name__)
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-#app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MODEL_FOLDER'] = 'BD-Model'
-#app.config['NB_FOLDER'] = 'Notebooks'
 
 model = None
 graph = None
@@ -34,7 +32,7 @@ def load_model():
     global graph
     # the following with stmnt may only be needed when running in tensorflow 1.13.1 environment.
     with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
-        model = keras.models.load_model(os.path.join(app.config['MODEL_FOLDER'], "cnn1_trained_jn.h5"))
+        model = keras.models.load_model(os.path.join(app.config['MODEL_FOLDER'], "cnn1_augmented_extra_v2.h5"))
     graph = K.get_session().graph
     print("  *****  Model loaded!")
 
